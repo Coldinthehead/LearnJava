498a954346f0f254299f5d879ef0bd5850544277
Coldinthehead <20034749+Coldinthehead@users.noreply.github.com>
2024-01-05 10:37:04 +0200

    Fix formatting

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,11 +2,10 @@ package org.example;
 
 import org.apache.log4j.*;
 
-public class App
-{
+public class App {
     private static Logger logger = Logger.getLogger(App.class);
-    public static void main( String[] args )
-    {
+
+    public static void main(String[] args) {
         BasicConfigurator.configure();
         logger.log(Priority.DEBUG, "Debug logger message");
         logger.log(Priority.INFO, "Info logger message");

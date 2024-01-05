cc7344b88ba81dc07972ee05b95e3532458fafab
Coldinthehead <20034749+Coldinthehead@users.noreply.github.com>
2024-01-05 10:24:35 +0200

    Add logger

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,24 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.apache.log4j.*;
+
+public class App
 {
+    private static Logger logger = Logger.getLogger(App.class);
     public static void main( String[] args )
     {
-        System.out.println("Hello world");
+        BasicConfigurator.configure();
+        logger.log(Priority.DEBUG, "Debug logger message");
+        logger.log(Priority.INFO, "Info logger message");
+        logger.log(Priority.ERROR, "Error logger message");
+
+        logger.setLevel(Level.DEBUG);
+        logger.debug("Debug logger message");
+        logger.info("info after debug message");
+        logger.setLevel(Level.INFO);
+        logger.info("Info logger message");
+        logger.debug("debug after info");
+        logger.setLevel(Level.ERROR);
+        logger.error("Error logger message");
     }
 }
